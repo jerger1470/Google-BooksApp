01230c59467cb5009f213da4ef1ede6c64c05bf0
Bryan Jerger <Bjerger1470@gmail.com>
2020-03-28 20:51:15 -0500

    small update to env and server

--- server.js
+++ server.js
@@ -1,13 +1,26 @@
 const express = require("express");
 const routes = require("./routes");
+const mongoose = require("mongoose");
 
 const PORT = process.env.PORT || 3001;
 const app = express();
 
-// Define middleware here
+// Set up middleware here
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
+//Connect to MongoDB Cloud Atlas or local mongoDB
+mongoose.connect(
+    process.env.MONGODB_URL || "mongodb://localhost/googlebooks",
+  {
+    useNewUrlParser: true,
+    useFindAndModify: false,
+    useUnifiedTopology: true
+  },
+  
+  () =>
+    console.log("connected to DB!")
+  );
 // Define API routes here
 app.use(routes)
 
