fdebc14127b4948a5315ea35f768fd098680fcba
Bryan Jerger <Bjerger1470@gmail.com>
2020-03-28 23:45:16 -0500

    created build folder

---
+++ client/build/precache-manifest.0663ac39a18a0289527556d35d662b05.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "dd00201edbdd1207693f27ddf63241c3",
+    "url": "/index.html"
+  },
+  {
+    "revision": "79d60c6db61c7ce63b6b",
+    "url": "/static/css/main.e139d740.chunk.css"
+  },
+  {
+    "revision": "30cb92287f4f2ab3280b",
+    "url": "/static/js/2.0cfdc741.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.0cfdc741.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "79d60c6db61c7ce63b6b",
+    "url": "/static/js/main.76fbd6b0.chunk.js"
+  },
+  {
+    "revision": "39b24b89a823cd86bca2",
+    "url": "/static/js/runtime-main.602a58f2.js"
+  },
+  {
+    "revision": "74165250b756792d6f08d55412a645de",
+    "url": "/static/media/library.74165250.jpg"
+  }
+]);
